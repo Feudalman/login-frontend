5716ca9fa17d580e4f9b2835072e56b269b93f52
Feudalman <1575079435@qq.com>
2023-01-19 23:02:47 +0800

    Update LoginBlock.jsx

--- src/views/Login/components/LoginBlock/LoginBlock.jsx
+++ src/views/Login/components/LoginBlock/LoginBlock.jsx
@@ -1,15 +1,31 @@
 import "../Block.scss";
 
+// 输入框组件使用的是antd组件库中的
 import { Input } from "antd";
+// 函数式组件，需要引入Hook useState
 import React, { useState } from "react";
 
+/**
+ * 因为需要使用ref传参，所以使用React.forwardRef
+ * 现在推荐使用函数式组件，类式组件比较冗杂
+ */
 const LoginBlock = React.forwardRef((props, ref) => {
+  // 用户信息
   let [userInfo, setUserInfo] = useState({
     account: "",
     password: "",
   });
 
+  // 输入触发，使用受控输入
   const handleInput = (e, type) => {
+    /**
+     * 这里为什么要这样写呢？
+     * 因为如果修改userInfo，就是在响应式原型上面修改，没有作用
+     * 如果直接 let temp = userInfo，虽然变量不同，但是都指向同一个地址
+     * 修改temp和修改userInfo其实本质都一样
+     * 所以现在要是用深拷贝，如果不了解，可以看看浅/深拷贝
+     * 还可以看看这篇博客：https://blog.csdn.net/qq_51574759/article/details/128487007?spm=1001.2014.3001.5501
+     */
     let temp = JSON.parse(JSON.stringify(userInfo));
     if (type === "account") {
       temp.account = e.target.value;
